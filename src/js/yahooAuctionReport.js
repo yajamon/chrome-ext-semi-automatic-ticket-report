b26c74619d67b3f725050065b2830836bd03ec78
yajamon <yajamon.tatsuki@gmail.com>
2016-07-24 14:37:34 +0900

    Update content script

--- src/js/yahooAuctionReport.js
+++ src/js/yahooAuctionReport.js
@@ -2,12 +2,28 @@
 /**
  * @type {NodeListOf<HTMLInputElement>}
  */
-var radios = document.getElementsByName("violation_code");
-for (var index = 0; index < radios.length; index++) {
-  var element = radios[index];
-  if (element.value !== "other") {
-    continue;
+const radios = document.getElementsByName("violation_code");
+for (let index = 0; index < radios.length; index++) {
+  let element = radios[index];
+  if (element.value === "other") {
+    element.click();
+    break;
   }
-  element.click();
-  break;
+}
+// その他の内容を選択
+/**
+ * @type {HTMLSelectElement}
+ */
+const select = document.getElementsByName("other_violation_code").item(0);
+for (let index = 0; index < select.options.length; index++) {
+  /**
+   * @type {HTMLOptionElement}
+   */
+  let element = select.options[index];
+  let regexp = /特定.*商品.*規定/g;
+  console.log(element.innerText);
+  if (element.innerText.match(regexp)) {
+    element.selected = true;
+    break;
+  }
 }
